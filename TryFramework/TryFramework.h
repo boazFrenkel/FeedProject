0d10082555db940b052b15c0e2d1c8cc7fb94945
boazFrenkel <boazfrenkel@gmail.com>
2020-08-20 21:46:54 +0300

    Initial Commit

---
+++ TryFramework/TryFramework.h
@@ -0,0 +1,19 @@
+//
+//  TryFramework.h
+//  TryFramework
+//
+//  Created by Boaz Frenkel on 20/08/2020.
+//  Copyright © 2020 BoazFrenkel. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TryFramework.
+FOUNDATION_EXPORT double TryFrameworkVersionNumber;
+
+//! Project version string for TryFramework.
+FOUNDATION_EXPORT const unsigned char TryFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TryFramework/PublicHeader.h>
+
+
