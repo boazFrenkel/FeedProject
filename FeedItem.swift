c033d6ccac15252c2299091fbc24ba121bb301ca
boazFrenkel <boazfrenkel@gmail.com>
2020-08-20 21:49:32 +0300

    Create the feed item model

---
+++ FeedItem.swift
@@ -0,0 +1,16 @@
+//
+//  FeedItem.swift
+//  TryFramework
+//
+//  Created by Boaz Frenkel on 20/08/2020.
+//  Copyright © 2020 BoazFrenkel. All rights reserved.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
