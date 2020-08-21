f6c20909f74559347818f514a85361144df146c5
boazFrenkel <boazfrenkel@gmail.com>
2020-08-21 17:39:57 +0300

    remove duplications

--- TryFrameworkTests/RemoteFeedLoaderTests.swift
+++ TryFrameworkTests/RemoteFeedLoaderTests.swift
@@ -9,74 +9,54 @@
 import XCTest
 
 class RemoteFeedLoader {
-    var httpClient: HTTPClientProtocol
-    
-    init(httpClient: HTTPClientProtocol) {
+    var httpClient: HTTPClient
+    var url: URL
+    init(url: URL, httpClient: HTTPClient) {
+        self.url = url
         self.httpClient = httpClient
     }
     
     func load() {
-        let url = URL(string: "https://blabla.com")
-        httpClient.get(from: url!)//requestedURL = URL(string: "https://blabla.com")
+        // let url = URL(string: "https://blabla.com")
+        httpClient.get(from: url)//requestedURL = URL(string: "https://blabla.com")
     }
-//    func load(complition: @escaping (LoadFeedResult) -> Void) {
-//        self.httpClient.requestedURL = URL()
-//    }
+    //    func load(complition: @escaping (LoadFeedResult) -> Void) {
+    //        self.httpClient.requestedURL = URL()
+    //    }
 }
 
-protocol HTTPClientProtocol {
-    //var requestedURL: URL? {get set}
-    
+protocol HTTPClient {
     func get(from url: URL)
-}
-
-class HTTPClient: HTTPClientProtocol {
     
-    
-    func get(from url: URL) { }
-}
-
-class HTTPClientSpy: HTTPClientProtocol {
-    var requestedURL: URL?
-    
-    func get(from url: URL) {
-        requestedURL = url
-    }
 }
 
 class RemoteFeedLoaderTests: XCTestCase {
-
+    
     func test_init_doesNotRequestDataFromURL() {
-        let client = HTTPClientSpy()
-        _ = RemoteFeedLoader(httpClient: client)
+        let (_, client) = makeSUT()
         XCTAssertNil(client.requestedURL)
     }
     
     func test_load_requestDataFromURL() {
-        let client = HTTPClientSpy()
-        let sut = RemoteFeedLoader(httpClient: client)
+        let (sut, client) = makeSUT()
         sut.load()
         
-        XCTAssertNotNil(client.requestedURL)
+        XCTAssertEqual(client.requestedURL, sut.url)
     }
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    
+    //MARK: - Helpers
+    private func makeSUT(url: URL = URL(string: "https://blabla.com")!, client: HTTPClient = HTTPClientSpy()) -> (sut: RemoteFeedLoader, client: HTTPClientSpy) {
+        let client = HTTPClientSpy()
+        let remoteFeedLoader = RemoteFeedLoader(url: url, httpClient: client)
+        return (remoteFeedLoader, client)
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    
+    private class HTTPClientSpy: HTTPClient {
+        var requestedURL: URL?
+        
+        func get(from url: URL) {
+            requestedURL = url
         }
     }
-
+    
 }
