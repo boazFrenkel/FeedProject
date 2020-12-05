5052e8d07217cd86ecf26916df4aad859ed2e358
boazFrenkel <boazfrenkel@gmail.com>
2020-12-05 12:39:58 +0200

    refactor

---
+++ Feed Feature/Feed API/FeedItemsMapper.swift
@@ -0,0 +1,45 @@
+//
+//  FeedItemsMapper.swift
+//  TryFramework
+//
+//  Created by Boaz Frenkel on 05/12/2020.
+//  Copyright © 2020 BoazFrenkel. All rights reserved.
+//
+
+import Foundation
+
+public class FeedItemsMapper {
+    
+    private struct FeedItemDTO: Decodable {
+        public let id: UUID
+        public let description: String?
+        public let location: String?
+        public let image: URL
+        
+        public init(id: UUID, description: String?, location: String?, imageURL: URL) {
+            self.id = id
+            self.description = description
+            self.location = location
+            self.image = imageURL
+        }
+        
+        var feedItem: FeedItem {
+            return FeedItem(id: id, description: description, location: location, imageURL: image)
+        }
+        
+    }
+    
+    private struct FeedItemsResponse: Decodable {
+        var items: [FeedItemDTO]
+    }
+    
+    static func map(data: Data, response: HTTPURLResponse) throws -> [FeedItem] {
+        guard response.statusCode == 200 else {
+            throw RemoteFeedLoader.RemoteError.invalidData
+        }
+        
+        let decoder = JSONDecoder()
+        let items = try decoder.decode(FeedItemsResponse.self, from: data).items
+        return items.map { $0.feedItem }
+    }
+}
